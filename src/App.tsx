41d6c578133eb93c289a77c530b584241bba5812
javohir7777 <javohirjumayev7777@gmail.com>
2023-10-17 15:15:35 -0700

    full project

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,32 @@ import CategoriesPage from "./pages/CategoriesPage";
 
 import { AuthContext } from "./context/AuthContext";
 import CardId from "./components/card/CardId";
+import AdminLayout from "./components/layout/Layout";
 
 function App() {
   const { isAuthenticated } = useContext(AuthContext);
   return (
     <BrowserRouter>
       <Routes>
+        {isAuthenticated ? (
+          <Route path="/" element={<AdminLayout />}>
+            <Route path="/categories" element={<CategoriesPage />} />
+            <Route path="/categorie/:idx" element={<CardId />} />
+          </Route>
+        ) : null}
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/categorie/:idx" element={<CardId />} />
+
         <Route
           path="/categories"
           element={
             isAuthenticated ? <CategoriesPage /> : <Navigate to="/login" />
           }
-        />
-        <Route path="/categorie/:idx" element={<CardId />} />
+        /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,96 @@
-import { ChildrenType } from "../../types/children";
+import { useContext, useState } from "react";
 
-const Layout = ({ children }: ChildrenType) => {
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  DollarOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+
+import { Layout, Menu, Button, theme } from "antd";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+
+// import "./Header.scss";
+import { TOKEN } from "../../constants";
+import { AuthContext } from "../../context/AuthContext";
+import { AuthContextTypes } from "../../types/auth-context";
+
+const { Header, Sider, Content } = Layout;
+
+const AdminLayout = () => {
+  const location = useLocation();
+  const { setIsAuthenticated }: AuthContextTypes = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const logout = () => {
+    localStorage.removeItem(TOKEN);
+    setIsAuthenticated(false);
+    navigate("/");
+  };
   return (
-    <div>
-      <header></header>
-      {children}
-     
-    </div>
+    <Layout className="admin-layout">
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo">Portfolio admin</div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={[location.pathname]}
+          items={[
+            {
+              key: "/categories",
+              icon: <DollarOutlined style={{ fontSize: "20px" }} />,
+              label: (
+                <Link className="text-decoration-none" to="/skills">
+                  Categories
+                </Link>
+              ),
+            },
+            {
+              key: "/",
+              icon: <LogoutOutlined style={{ fontSize: "20px" }} />,
+              label: (
+                <Button type="link" style={{ color: "#fff" }} onClick={logout}>
+                  Logout
+                </Button>
+              ),
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
-
-export default Layout;
+export default AdminLayout;
